17e70e0e6e278f1cb4f9265d58806b63bac74f96
Rupesh Tayde <126845760+RT077@users.noreply.github.com>
2025-06-03 14:09:47 +0000

    3rd commit

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+//let temp="33";
+//let temp="33abc";
+//let temp=null;
+//let temp=undefined;   NaN
+console.log(typeof temp); //String 
+console.log(typeof (temp)); //String
+
+let valueOfTemp =Number(temp);
+console.log(typeof valueOfTemp); //number
+console.log(valueOfTemp);
+
+/*
+"33"=> 33
+"33abc"=> NaN
+true =>1 or false=> 0
+*/
