65b373034bb45d4887235a7dc4d9c681b5949dd3
Rupesh Tayde <126845760+RT077@users.noreply.github.com>
2025-06-03 14:09:30 +0000

    4th commit

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,9 @@
+console.log(null >0);
+console.log(null <0);
+console.log(null ==0);
+console.log(null >=0);
+// avoid above comparision
+
+// Strikt check ===
+console.log("2"===2);// not same tow diff data types
+
