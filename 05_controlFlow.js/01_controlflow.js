fbe4a98de3e468ee8ec8528656dff2de1b108082
Rupesh Tayde <126845760+RT077@users.noreply.github.com>
2025-06-13 19:55:26 +0000

    control flow commit

---
+++ 05_controlFlow.js/01_controlflow.js
@@ -0,0 +1,33 @@
+// if
+
+const isUserLoggedIn=true;
+
+if(isUserLoggedIn==true){
+    console.log(`hello world`);
+}
+/*
+< less than 
+> greater than
+<= less then equal
+>= greater then equal
+== is equal to
+!= not equal 
+=== strict equal or tripal equal ,	equal value and equal type
+!== not equal value or not equal type
+*/ 
+
+// const temparture=51;
+
+// if (temparture ==41){
+//     console.log(`the temprature is less than 50 degr`);
+// }
+// else{
+//     console.log(`the temprature is more than 50 degr`); 
+// }
+
+const score=200;
+if(score>100){
+    const power="fly";
+    console.log(`user power : ${power}`);
+    
+}
